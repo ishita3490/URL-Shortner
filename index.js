955fc38da9a4971ec97eaf3d21fdfd0e89901566
Ishita Bansal <ishitabansal770@gmail.com>
2024-03-01 20:22:04 +0530

    added changes

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
 const express = require("express");
 const { connectToMongoDB } = require("./connect");
 const urlRoute = require("./routes/url");
+const URL = require('./models/url');
 
 const app = express();
+const PORT = 8003;
 
-connectToMongoDB("mongodb://localhost:27017/short_url")
-    .then(() => {
-        console.log("MongoDB connected");
-        
-        // Start the server once MongoDB is connected
-        const server = app.listen(3000, () => {
-            console.log(`Server Started at PORT:${server.address().port}`);
-        });
-    })
+connectToMongoDB("mongodb://localhost:27017/short-url")
+    .then(() => console.log("MongoDB Connected"))
     .catch(err => console.error("Error connecting to MongoDB:", err));
 
+app.use(express.json());
+
+app.get('/:shortId', async (req, res) => {
+    const shortId = req.params.shortId;
+    const entry = await URL.findOneAndUpdate({
+        shortId
+    }, {
+        $push: {
+            visitedHistory: {
+                timestamp: Date.now(),
+            },
+        },
+    });
+    res.redirect(entry.redirectURL);
+});
+
 app.use("/url", urlRoute);
+
+app.listen(PORT, () => console.log(`Server Started at PORT:${PORT}`));
